1973b5045990db119c6f86b97da635a045e0f986
Jákup Svøðstein <jakupsv@setur.fo>
2023-02-08 12:18:04 +0000

    init lightning-hydra-template

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Describe Your Cool Project",
+    author="jákup Svøðstein",
+    author_email="jakupsv@setur.fo",
+    url="https://github.com/Marimuda/FluNet",
+    install_requires=["pytorch-lightning", "hydra-core"],
+    packages=find_packages(),
+)
