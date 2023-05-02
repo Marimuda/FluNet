0cf0147d76cb17bff9ee3ef3dd65da1039a3fc03
jakup <marimuda90@gmail.com>
2023-05-02 15:49:11 +0100

    added comment

--- src/eval.py
+++ src/eval.py
@@ -72,6 +72,7 @@ def evaluate(cfg: DictConfig) -> Tuple[dict, dict]:
     log.info("Starting testing!")
     trainer.test(model=model, datamodule=datamodule, ckpt_path=cfg.ckpt_path)
 
+    # Code commented out
     # for predictions use trainer.predict(...)
     # predictions = trainer.predict(model=model, dataloaders=dataloaders, ckpt_path=cfg.ckpt_path)
 
