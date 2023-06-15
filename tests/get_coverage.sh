97c6131e337722ac2d137702c5e2adac6448f6a3
jakupsv <jakupsv@setur.fo>
2023-06-15 15:53:59 +0200

    TORCH-26 Updated setup.py to import package from new location to import flunet/ instead of src/flunet

---
+++ tests/get_coverage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###############################################################################
+# This is a revision based on a script taken from the "modulus" repository (Apache 2.0 License)
+# Repository: https://github.com/NVIDIA/modulus
+# License: Apache 2.0
+# 
+# Description: This script executes coverage checks using `coverage` and `pytest`.
+#              It runs the tests, combines the coverage data, generates a coverage
+#              report, and cleans up the generated coverage files.
+##############################################################################
+
+# do the coverage checks
+coverage run \
+--rcfile='coverage.pytest.rc' \
+-m pytest \
+
+coverage run \
+--rcfile='coverage.docstring.rc' \
+-m pytest \
+--doctest-modules ../src/flunet//
+
+coverage combine --data-file=.coverage
+coverage report --omit=*test*
+
+# if you wish to view the report in HTML format uncomment below
+coverage html --omit=*test*
+
+# cleanup
+rm .coverage
